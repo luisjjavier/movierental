8525c2755293035f2e35de5693ab0b35ca209f25
LuisJavier <javier.luis29@gmail.com>
2018-04-04 18:56:06 -0400

    Added SB template

--- Webapp/App_Start/BundleConfig.cs
+++ Webapp/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Webapp
 {
@@ -9,7 +8,7 @@ namespace Webapp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/vendor/jquery/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,12 +19,20 @@ namespace Webapp
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Content/vendor/bootstrap/js/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+            bundles.Add(new StyleBundle(@"~/Content/css").Include(
+                      "~/Content/vendor/bootstrap/css/bootstrap.css",
+                      "~/Content/vendor/metisMenu/metisMenu.min.css",
+                      "~/Content/dist/css/sb-admin-2.css",
+                     "~/Content/vendor/font-awesome/css/font-awesome.min.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/pagescripts").Include(
+                "~/Content/vendor/metisMenu/metisMenu.js",
+                "~/Content/dist/js/sb-admin-2.js"));
+               
         }
     }
 }
